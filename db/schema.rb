7df2aa7fee8c5a4b15623e774951620d868fa7bc
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-02-19 18:59:55 +0900

    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_090922) do
+ActiveRecord::Schema.define(version: 2021_02_19_093319) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
